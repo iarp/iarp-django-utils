92c97345bf03cee0b493e335d886ee1a696da684
iarp <ian@iarp.ca>
2020-04-27 19:07:24 -0400

    forgot to add pagination template_contexts.py

---
+++ iarp_django_utils/template_contexts.py
@@ -0,0 +1,12 @@
+from django.conf import settings
+
+
+def add_pagination_settings(request=None):
+    return {
+        'pagination_include_separator': getattr(settings, 'PAGINATION_INCLUDE_SEPARATOR', None),
+        'pagination_include_first': getattr(settings, 'PAGINATION_INCLUDE_FIRST', None),
+        'pagination_include_last': getattr(settings, 'PAGINATION_INCLUDE_LAST', None),
+        'pagination_neighbors': getattr(settings, 'PAGINATION_NEIGHBORS', None),
+        'pagination_button_classes': getattr(settings, 'PAGINATION_BUTTON_CLASSES', 'btn btn-sm btn-secondary'),
+        'pagination_include_last_page_in_last_button': getattr(settings, 'PAGINATION_INC_LAST_PAGE_IN_LAST_BTN', True),
+    }
