017cb46fe6d967f8cb94c2bc62504da3e2b7f07e
iarp <iarp.opensource@gmail.com>
2020-12-03 20:30:57 -0500

    fix(pagination): update to match bootstrap 4+ format

--- iarp_django_utils/template_contexts.py
+++ iarp_django_utils/template_contexts.py
@@ -7,7 +7,6 @@ def add_pagination_settings(request=None):
         'pagination_include_first': getattr(settings, 'PAGINATION_INCLUDE_FIRST', None),
         'pagination_include_last': getattr(settings, 'PAGINATION_INCLUDE_LAST', None),
         'pagination_neighbors': getattr(settings, 'PAGINATION_NEIGHBORS', None),
-        'pagination_button_classes': getattr(settings, 'PAGINATION_BUTTON_CLASSES', 'btn btn-sm btn-secondary'),
         'pagination_include_last_page_in_last_button':
             getattr(settings, 'PAGINATION_INCLUDE_LAST_PAGE_IN_LAST_BUTTON', True),
     }
