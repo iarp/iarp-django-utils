b6235ea62d934f04bc5cda88d0e1c83a3958a03b
iarp <iarp.opensource@gmail.com>
2021-03-15 14:47:46 -0400

    version dump v0.0.8

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='iarp_django_utils',
-    version='0.0.7',
+    version='0.0.8',
     description='A personal collection of common django utilities used in various projects',
     url='https://bitbucket.org/iarp/iarp-django-utils/',
     author='IARP',
