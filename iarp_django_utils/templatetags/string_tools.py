3c18de0de7cf9dd2ef811c780d992b26afe8093e
iarp <iarp.opensource@gmail.com>
2022-07-16 18:56:34 -0400

    feat(string_tools): add replace as an available template filter

--- iarp_django_utils/templatetags/string_tools.py
+++ iarp_django_utils/templatetags/string_tools.py
@@ -38,3 +38,16 @@ def truncatesentences(value, arg):
 
         lines.append(line)
     return '\n'.join(lines)
+
+
+@register.filter
+def replace(value, arg):
+    """
+    Replacing filter
+    Use `{{ "aaa"|replace:"a|b" }}`
+    """
+    if len(arg.split('|')) != 2:
+        return value
+
+    what, to = arg.split('|')
+    return value.replace(what, to)
