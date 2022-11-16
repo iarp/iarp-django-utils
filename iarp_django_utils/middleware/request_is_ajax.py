6fdc2f8805fe18f6aa838d6bf2e4f4b3dff102b0
iarp <iarp.opensource@gmail.com>
2022-11-16 18:36:35 -0500

    feat(middleware): add system to bring back request.is_ajax()

---
+++ iarp_django_utils/middleware/request_is_ajax.py
@@ -0,0 +1,19 @@
+
+
+class AddRequestIsAjax(object):
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        def is_ajax():
+            return any([
+                request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest",
+                request.content_type == "application/json",
+                request.META.get("HTTP_ACCEPT") == "application/json",
+                request.META.get('HTTP_HX_REQUEST'),
+            ])
+
+        request.is_ajax = is_ajax
+
+        return self.get_response(request)
