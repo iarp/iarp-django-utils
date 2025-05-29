6fddc57c2d7f501e831269d0e2103e58f76bc750
iarp <iarp.opensource@gmail.com>
2025-05-29 10:00:05 -0400

    change: convert setup.py to pyproject.toml

--- setup.py
+++ setup.py
@@ -1,20 +1,5 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name='iarp_django_utils',
-    version='0.0.9',
-    description='A personal collection of common django utilities used in various projects',
-    url='https://bitbucket.org/iarp/iarp-django-utils/',
-    author='IARP',
-    author_email='iarp.opensource@gmail.com',
-    license='MIT',
-    packages=find_packages(exclude=['example'], include=['iarp_django_utils*']),
-    install_requires=[
-        'django',
-    ],
-    dependency_links=[
-        # Make sure to include the `#egg` portion so the `install_requires` recognizes the package
-        'git+https://iarp@bitbucket.org/iarp/iarp-python-utils.git#egg=iarp_utils'
-    ],
-    zip_safe=False
-)
+
+if __name__ == "__main__":
+    setup()
