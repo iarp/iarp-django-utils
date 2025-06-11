f45320dd232ce60108f3747b1a01b11856c10cec
iarp <iarp.opensource@gmail.com>
2025-06-11 08:41:22 -0400

    change: init was loading a configure file, no longer.

--- iarp_django_utils/__init__.py
+++ iarp_django_utils/__init__.py
@@ -1,17 +1,5 @@
-import os
-
 import django
-from django.conf import settings
-
-from iarp_utils import configuration
 
 
 if django.VERSION < (4, 0):
     default_app_config = "iarp_django_utils.apps.IarpDjangoUtilsConfig"
-
-UTILS_CONFIG_FILE = os.path.join(settings.BASE_DIR, "IARPUTILS.json")
-CONFIG = configuration.load(UTILS_CONFIG_FILE)
-
-
-def save_config(config):
-    configuration.save(config=config, file_location=UTILS_CONFIG_FILE)
